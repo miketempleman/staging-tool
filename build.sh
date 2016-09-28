758f90ac1c04713a75868807cc7382483706b100
miketempleman <mike@meshfire.com>
2016-09-28 08:45:29 -1000

    comments

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # THIS IS ONLY FOR OSX
 # To build a single file executable for Linux run this script on the python files
 # on the linux target machine. I have no idea how to do this on Windoze
-
+#
 pyinstaller --name staging --onefile main.py
 
 pyinstaller staging.spec
